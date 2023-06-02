0b40c0b4bd6591a1473d90faa64eaf9d655e55fe
李衡 <li15038043160@163.com>
2023-06-02 16:54:34 +0800

    auctions脚本优化

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 !#/bin/bash
-set -e
-
-cd /workspace/blog
-git pull
 docker run --rm -v /workspace/blog:/app -w /app node:16 sh -c 'npm install --registry=https://registry.npm.taobao.org && npm run build'
