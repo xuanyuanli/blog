888410c5aa34e11c9f423b5a2e79199643d7207f
李衡 <li15038043160@163.com>
2023-06-14 16:43:25 +0800

    脚本优化

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 docker run --rm -v /workspace/blog:/app -w /app node:16 sh -c 'npm install --registry=https://registry.npm.taobao.org && npm run build'
+
+# 复制文件到www目录（防止构建期间访问出错，注意Nginx的配置目录为www目录）
+if [ ! -d /var/www/blog ];then
+  mkdir -p /var/www/blog
+fi
+cp -rfu /workspace/blog/docs/.vuepress/dist/* /var/www/blog
