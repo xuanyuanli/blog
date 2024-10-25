f6d7e635bcdf71610d63b23d348b3a55746cd09a
xuanyuanli <xuanyuanli999@gmail.com>
2024-10-25 11:26:25 +0800

    cnd 替换

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --rm -v /workspace/blog:/app -w /app node:18 sh -c 'npm install --registry=https://registry.npmmirror.com && npm run build'
+docker run --rm -v /workspace/blog:/app -w /app node:16 sh -c 'npm install --registry=https://registry.npmmirror.com && npm run build'
 
 # 复制文件到www目录（防止构建期间访问出错，注意Nginx的配置目录为www目录）
 if [ ! -d /var/www/blog ];then
